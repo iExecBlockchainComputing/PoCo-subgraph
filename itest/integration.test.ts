253e9656da3849e568abb7ad67b6928880a2cc84
Jérémy James Toussaint <33313130+jeremyjams@users.noreply.github.com>
2024-10-21 15:06:10 +0200

    Use testcontainers for itest

--- itest/integration.test.ts
+++ itest/integration.test.ts
@@ -1,10 +1,22 @@
 import { ApolloClient, InMemoryCache, gql } from '@apollo/client';
 import assert from 'assert';
+import { DockerComposeEnvironment, Wait } from 'testcontainers';
 
 const APIURL = 'http://localhost:8000/subgraphs/name/test/poco';
 
 async function main() {
     console.log('Running integration tests..');
+    const environment = new DockerComposeEnvironment(
+        'docker/test/',
+        'docker-compose.yml',
+    ).withWaitStrategy(
+        'poco-subgraph-deployer-1',
+        Wait.forLogMessage('Created subgraph: test/poco'),
+    );
+    await environment.up();
+    const secondsToWait = 20;
+    console.log(`Waiting ${secondsToWait}s..`);
+    await new Promise((resolve) => setTimeout(resolve, secondsToWait * 1000));
     const client = new ApolloClient({
         uri: APIURL,
         cache: new InMemoryCache(),
@@ -22,7 +34,7 @@ async function main() {
     const protocol = result.data.protocol;
     assert.equal(protocol.id, 'iExec');
     assert.equal(protocol.tvl, '0.02025');
-    console.log('integration tests completed ✔️');
+    console.log('Completed integration tests ✔️');
 }
 
 main();
